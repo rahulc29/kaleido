9b6aeac6e2afebb370e2d56849230c4acf8ab4e7
rahulc29 <rahul29112002@gmail.com>
2021-10-29 14:47:01 +0530

    Remove old API and implementations

--- lexer/RegexLexer.cpp
+++ lexer/RegexLexer.cpp
@@ -1,46 +1,5 @@
 #include "RegexLexer.h"
 #include <set>
-std::unique_ptr<kaleido::Token> kaleido::RegexLexer::nextToken() const {
-    std::string nextPotentialToken;
-    // check for end of file before requesting string processing from stream
-    if (mInput.peek()==EOF) {
-        return std::make_unique<Token>(Token::END_OF_FILE_TOKEN);
-    }
-    mInput >> nextPotentialToken;
-    std::unique_ptr<Token> toReturn;
-    if (!nextPotentialToken.empty()) {
-        // firstly, ignore the comments
-        if (std::regex_match(nextPotentialToken, IS_COMMENT)) {
-            // recursor input stream to next line
-            mInput.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            return nextToken();
-        }
-        // check for keywords
-        if (std::regex_match(nextPotentialToken, IS_KEYWORD)) {
-            toReturn = std::make_unique<Token>(Token(nextPotentialToken, TokenType::TOKEN_KEYWORD));
-            return toReturn;
-        }
-        // check if number
-        if (std::regex_match(nextPotentialToken, IS_NUMBER)) {
-            toReturn = std::make_unique<Token>(Token(nextPotentialToken, TokenType::TOKEN_NUMBER));
-            return toReturn;
-        }
-        // check for operators
-        if (std::regex_match(nextPotentialToken, IS_OPERATOR)) {
-            toReturn = std::make_unique<Token>(Token(nextPotentialToken, TokenType::TOKEN_OPERATOR));
-            return toReturn;
-        }
-        // check for identifiers
-        if (std::regex_match(nextPotentialToken, IS_IDENTIFIER)) {
-            toReturn = std::make_unique<Token>(Token(nextPotentialToken, TokenType::TOKEN_IDENTIFIER));
-            return toReturn;
-        }
-        // no regex matches
-        toReturn = std::make_unique<Token>(Token(nextPotentialToken, TokenType::TOKEN_UNSPECIFIED));
-    }
-    return toReturn;
-}
-
 std::regex kaleido::RegexLexer::IS_IDENTIFIER = std::regex("[a-zA-Z_][a-zA-Z0-9_]*");
 std::regex kaleido::RegexLexer::IS_NUMBER = std::regex("[0-9]+\\.?[0-9]*");
 std::regex kaleido::RegexLexer::IS_KEYWORD = std::regex("def|extern");
