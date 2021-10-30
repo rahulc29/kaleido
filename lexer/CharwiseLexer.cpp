f0000d019db147b188f628b0a92d1998f5df9aa5
rahulc29 <rahul29112002@gmail.com>
2021-10-30 20:15:40 +0530

    Refactor tests into separate directory

--- lexer/CharwiseLexer.cpp
+++ lexer/CharwiseLexer.cpp
@@ -1,6 +1,3 @@
 #include "CharwiseLexer.h"
-std::unique_ptr<kaleido::Token> kaleido::CharwiseLexer::nextToken() const {
-
-}
 kaleido::CharwiseLexer::CharwiseLexer(std::istream &input) : mInput(input) {
 }
